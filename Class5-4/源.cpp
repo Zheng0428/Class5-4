9b19ab9cb1a6945264dc306835b180490031e8ec
天昱 郑 <1219979760@qq.com>
2020-03-27 14:26:57 +0800

    1

--- Class5-4/源.cpp
+++ Class5-4/源.cpp
@@ -5,5 +5,21 @@ using namespace std;
 
 int main()
 {
-	waitKey(0);
+	VideoCapture cap(0);
+	while (1)
+	{
+		Mat frame;
+		Mat dx;
+		Mat dy;
+		Mat dxy;
+		cap >> frame;
+		Sobel(frame, dx, 0, 1, 0, 3, 1, 0, BORDER_DEFAULT);
+		Sobel(frame, dy, 0, 0, 1, 3, 1, 0, BORDER_DEFAULT);
+		Sobel(frame, dxy, 0, 1, 1, 3, 1, 0, BORDER_DEFAULT);
+		imshow("frame", frame);
+		imshow("dx", dx);
+		imshow("dy", dy);
+		imshow("dxy", dxy);
+		waitKey(30);
+	}
 }
